174e2613b981617e7f76524a62395ded707fec0a
KareemTaha0 <145080969+KareemTaha0@users.noreply.github.com>
2024-10-20 21:49:54 +0300

    Simple Arethmatic sequence
    Simple arethmatic sequence simulating a bank account!

---
+++ main.py
@@ -0,0 +1,8 @@
+#CALC AREA OF RECTANGLE
+
+M  = float(input("Enter Your Balance"))
+addedseq = float(input("Enter the amount you want to deposit "))
+NewBalance = M + addedseq
+
+print(f"Deposit Successfull ! your current balance is {NewBalance}$")
+
