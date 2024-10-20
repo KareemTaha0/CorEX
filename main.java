1e649fd995355fe7686def099d7aa5beb94c18af
KareemTaha0 <145080969+KareemTaha0@users.noreply.github.com>
2024-10-21 00:01:44 +0300

    Create main.java
    quick frame jav

---
+++ main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setSize(800, 600);
+        JPanel Graypane = new JPanel();
+
+    }
+}
