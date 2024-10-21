e0bfbf6bbdbb02dd1f9c340584448e55b8b38f24
Karim <145080969+KareemTaha0@users.noreply.github.com>
2024-10-21 22:47:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
     operator = input("Enter An opreator (+ - * /):")
-    num1 = float(input("Type The First number which calculates Vari x equals?"))
+    x = float(input("Type The First number which calculates Vari x equals?"))
     y = float(input("Type The 2nd number which calculates Vari y equals?"))
 
 
     if operator == "+":
-        print(num1 + y)
+        print(x + y)
     elif operator == "-":
-        print(num1 - y)
+        print(x - y)
 
     elif operator == "*":
-        print(num1 * y)
+        print(x * y)
 
     elif operator == "/":
         sww = input("What Number would u start with?(num1 y):")
         if sww == "num1":
-         print (num1 / y)
+         print (x / y)
 
 
         elif sww == "y" :
-            print(y / num1)
+            print(y / x)
 
